4b7d1bacfbd3998567ca9f59d4d1c9f44df0aff0
314NOTTI <nicolaspinotti.of@gmail.com>
2025-12-17 23:51:56 -0300

    Initial commit - Internal Helpdesk System backend

---
+++ main.py
@@ -0,0 +1,29 @@
+#1️⃣ database.py
+
+#conexão com SQLite
+
+#sessão do banco
+
+#2️⃣ models.py
+
+#User
+
+#Ticket
+
+#3️⃣ schemas.py
+
+#validação (o que entra / sai da API)
+
+#4️⃣ auth.py
+
+#hash de senha
+
+#JWT
+
+#login
+
+#5️⃣ main.py
+
+#rotas
+
+#integração de tudo
