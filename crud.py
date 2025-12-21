8a62b5a5a32fb0ecf61bdffc89c40d8823b0976c
314NOTTI <nicolaspinotti.of@gmail.com>
2025-12-20 23:12:43 -0300

    add CRUD operations for users

---
+++ crud.py
@@ -0,0 +1,32 @@
+from sqlalchemy.orm import Session
+
+from models import User
+from schemas import UserCreate
+
+def create_user(db: Session, user: UserCreate) -> User:
+    db_user = User(name=user.name, email=user.email)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def get_user_by_id(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(User).offset(skip).limit(limit).all()
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+def update_user(db: Session, db_user: User, name: str | None = None):
+    if name is not None:
+        db_user.name = name
+
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def delete_user(db: Session, db_user: User):
+    db.delete(db_user)
+    db.commit()
